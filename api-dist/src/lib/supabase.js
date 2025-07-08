39ee4fa309cbed08b72b6ed3aff209fa0c65cd15
b2bot <vagner@leadclinic.com.br>
2025-07-08 15:27:06 +0000

    Remove api-dist directory - simplify project structure

--- api-dist/src/lib/supabase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createClient = void 0;
-const supabase_js_1 = require("@supabase/supabase-js");
-const createClient = (url, key) => {
-    return (0, supabase_js_1.createClient)(url, key);
-};
-exports.createClient = createClient;
