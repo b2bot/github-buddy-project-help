bca4a033899d991ab28e05a7a2d5272b64a759b5
b2bot <vagner@leadclinic.com.br>
2025-07-08 05:41:41 -0300

    google-api

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-)
-
-export default supabase
+export const createClient = (url: string, key: string) => {
+  return createSupabaseClient(url, key)
+}
