c65c298bbd44a64551912e5fce0f9a05fc151cf1
b2bot <vagner@leadclinic.com.br>
2025-07-08 05:36:03 -0300

    google-api

---
+++ api/google-auth.ts
@@ -0,0 +1,30 @@
+import { google } from 'googleapis';
+
+const redirectUri = 'https://github-buddy-project-help.vercel.app/api/google-callback';
+
+export async function GET(req: Request) {
+  const oauth2Client = new google.auth.OAuth2(
+    '1015078541788-a38j0o12vm85m1bmddsbt40of7rul3r2.apps.googleusercontent.com',
+    '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',
+    redirectUri
+  );
+
+  const scopes = [
+    'https://www.googleapis.com/auth/webmasters.readonly',
+    'https://www.googleapis.com/auth/analytics.readonly',
+    'https://www.googleapis.com/auth/adwords',
+    'https://www.googleapis.com/auth/business.manage',
+	'https://www.googleapis.com/auth/userinfo.profile',
+	'https://www.googleapis.com/auth/analytics.edit',
+	'https://www.googleapis.com/auth/analytics.manage.users.readonly'
+	'https://www.googleapis.com/auth/analytics.user.deletion',
+  ];
+
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+    prompt: 'consent',
+  });
+
+  return Response.redirect(url);
+}
