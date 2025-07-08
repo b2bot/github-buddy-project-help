9e67c6431f128fd1176527b495ddd1adfefd5fa0
b2bot <vagner@leadclinic.com.br>
2025-07-08 07:09:04 -0300

    atualização local

--- api/google-auth.ts
+++ api/google-auth.ts
@@ -10,25 +10,15 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     redirectUri
   );
 
-
-const redirectUri = 'https://github-buddy-project-help.vercel.app/api/google-callback';
-
-export async function GET(req: Request) {
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID!,
-    process.env.GOOGLE_CLIENT_SECRET!,
-    redirectUri
-  );
-
   const scopes = [
     'https://www.googleapis.com/auth/webmasters.readonly',
     'https://www.googleapis.com/auth/analytics.readonly',
     'https://www.googleapis.com/auth/adwords',
     'https://www.googleapis.com/auth/business.manage',
-	'https://www.googleapis.com/auth/userinfo.profile',
-	'https://www.googleapis.com/auth/analytics.edit',
-	'https://www.googleapis.com/auth/analytics.manage.users.readonly',
-	'https://www.googleapis.com/auth/analytics.user.deletion',
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/analytics.edit',
+    'https://www.googleapis.com/auth/analytics.manage.users.readonly',
+    'https://www.googleapis.com/auth/analytics.user.deletion',
   ];
 
   const url = oauth2Client.generateAuthUrl({
@@ -37,5 +27,6 @@ export async function GET(req: Request) {
     prompt: 'consent',
   });
 
-  return Response.redirect(url);
+  res.writeHead(302, { Location: url });
+  res.end();
 }
